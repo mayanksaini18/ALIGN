82a8571530bd4e089f028d876e27868d8a9c1e02
Mayank Saini <mayanksaini0416@gmail.com>
2025-10-03 18:14:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-// import Header from './components/Header'; // This will be fixed in the next step
+ import Header from './components/Header'; // This will be fixed in the next step
 import Footer from './components/Footer';
 import Body from './components/Body';
 import Hero from './components/Hero';
@@ -7,7 +7,7 @@ import Hero from './components/Hero';
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-black text-white font-sans">
-      {/* <Header /> */}
+      <Header />
       <Hero />
       <Body className="flex-1" />
       <Footer />
