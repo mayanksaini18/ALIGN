c56345306b30c7f5b822385e9885cd574acd92f1
Mayank Saini <mayanksaini0416@gmail.com>
2025-10-03 17:57:05 +0530

    Create SplitTextSection.jsx

---
+++ src/components/SplitTextSection.jsx
@@ -0,0 +1,9 @@
+import SplitText from "./SplitText.jsx";
+
+export default function SplitTextSection() {
+    return (
+        <div className="flex items-center justify-center p-6" style={{ fontSize: '24px' }}>
+            <SplitText text="Hello, GSAP!" />
+        </div>
+    );
+}
