2cd50a2de424ffe70665d7b66a85bcea24b36a95
Mayank Saini <mayanksaini0416@gmail.com>
2025-10-04 03:33:33 +0530

    Delete SplitTextSection.jsx

--- src/components/SplitTextSection.jsx
+++
@@ -1,9 +0,0 @@
-import SplitText from "./SplitText.jsx";
-
-export default function SplitTextSection() {
-    return (
-        <div className="flex items-center justify-center p-6" style={{ fontSize: '24px' }}>
-            <SplitText text="Hello, GSAP!" />
-        </div>
-    );
-}
