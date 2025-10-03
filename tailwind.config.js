960bba8cd565f752dced8f69d8d69da415f718ce
Mayank Saini <mayanksaini0416@gmail.com>
2025-10-03 18:15:09 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'align-gold': '#FFC300',
+        'deep-panel': '#0A0A0A',
+      },
+      fontFamily: {
+        'serif': ['"Playfair Display"', 'serif'],
+        'sans': ['Inter', 'sans-serif'],
+        'mono': ['"Fira Code"', 'monospace']
+      }
+    },
   },
   plugins: [daisyui],
   daisyui: {
