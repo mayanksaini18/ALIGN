9e1f18830f7784b8812601a79f98633075fb8cc3
Mayank Saini <mayanksaini0416@gmail.com>
2025-10-03 17:56:48 +0530

    Create Body.jsx

---
+++ src/components/Body.jsx
@@ -0,0 +1,7 @@
+export default function Body() {
+    return (
+        <main className="p-6 flex-1">
+            <p className="text-lg">Welcome to my website! Here's some content.</p>
+        </main>
+    );
+}
