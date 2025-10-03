f29f4af34e5e5f1c010c5f55588e68a2c74137e1
Mayank Saini <mayanksaini0416@gmail.com>
2025-10-03 17:57:20 +0530

    Delete Body.jsx

--- src/pages/Body.jsx
+++
@@ -1,7 +0,0 @@
-export default function Body() {
-    return (
-        <main className="p-6 flex-1">
-            <p className="text-lg">Welcome to my website! Here's some content.</p>
-        </main>
-    );
-}
