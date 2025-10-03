ba98d6df6726335831bb86bdd002a82f9c5353b5
Mayank Saini <mayanksaini0416@gmail.com>
2025-10-03 17:58:28 +0530

    Delete Footer.jsx

--- src/pages/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="p-4 text-center">
-            &copy; 2025 My Website. All rights reserved.
-        </footer>
-    );
-}
