5d654cae2408c484405e8fd358e9568c45897950
Mayank Saini <mayanksaini0416@gmail.com>
2025-10-03 17:57:25 +0530

    Update Footer.jsx

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-gray-950 text-white p-4 text-center">
+        <footer className="p-4 text-center">
             &copy; 2025 My Website. All rights reserved.
         </footer>
     );
