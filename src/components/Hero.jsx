4d9dcd797b4c504eac3f5232626706c14abd5ccf
Mayank Saini <mayanksaini0416@gmail.com>
2025-10-04 03:33:29 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import TextType from "./TextType.jsx";
 export default function Hero() {
     return (
         <div className="flex flex-col items-center justify-center pt-24 pb-16 bg-black">
-            <SplitText text="ALIGN" />
+            <SplitText text="A.L.I.G.N"  />
             <div className="flex items-center justify-center p-6 mb-8 text-center mt-4">
                 <TextType
                     text={["Strategy that cuts through.", "Execution that drives growth.", "Results that redefine success."]}
@@ -13,6 +13,7 @@ export default function Hero() {
                     showCursor={true}
                     cursorCharacter="_"
                     deletingSpeed={40}
+
                 />
             </div>
             <a href="#contact" className="px-10 py-4 text-xl font-bold rounded-lg bg-align-gold text-black shadow-2xl hover:bg-white transition duration-300 transform hover:scale-[1.2] mt-8">
